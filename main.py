b5ae50d6851f4c69c21b7fe51249a8384f256528
Yehor Hubenko <112268293+YHubenko@users.noreply.github.com>
2023-12-16 02:09:53 +0200

    add classes using pattern Strategy and report for lab 4

--- main.py
+++ main.py
@@ -1,27 +1,161 @@
-from psycopg2 import sql
-from DatabaseConnection import get_database_connection, conn
-
-cur = conn.cursor()
-
-def insert_col(table_name, data):
-    insert_query = sql.SQL("INSERT INTO {} ({}) VALUES ({})").format(
-        sql.Identifier(table_name),
-        sql.SQL(', ').join(map(sql.Identifier, data.keys())),
-        sql.SQL(', ').join(sql.Placeholder() * len(data))
-    )
-    cur.execute(insert_query, list(data.values()))
-    conn.commit()
+import psycopg2
 
 
-data_to_insert = {
-    "name": "table",
-    "func": "<table></table>"
-}
-insert_col("macros", data_to_insert)
+class HintStrategy:
+    def get_hints(self, hints):
+        pass
 
-cur.execute('SELECT * FROM macros')
-result = cur.fetchall()
-print(result)
 
-cur.close()
-conn.close()
+class SimpleHintStrategy(HintStrategy):
+    def get_hints(self, hints):
+        return [hint.hint_text for hint in hints]
+
+
+class AdvancedHintStrategy(HintStrategy):
+    def get_hints(self, hints):
+        pass
+
+
+class SyntaxHighlighterStrategy:
+    def highlight(self, text):
+        pass
+
+
+class PythonSyntaxHighlighter(SyntaxHighlighterStrategy):
+    def highlight(self, text):
+        pass
+
+
+class DatabaseStrategy:
+    def connect(self, connection_params):
+        pass
+
+    def save_to_database(self, file_content):
+        pass
+
+
+class PostgreSQLDatabaseStrategy(DatabaseStrategy):
+    def __init__(self):
+        self.connection = None
+
+    def connect(self, connection_params):
+        self.connection = psycopg2.connect(**connection_params)
+
+    def save_to_database(self, file_content):
+        pass
+
+
+class BookmarkStrategy:
+    def process_bookmarks(self, bookmarks):
+        pass
+
+
+class DefaultBookmarkStrategy(BookmarkStrategy):
+    def process_bookmarks(self, bookmarks):
+        pass
+
+
+class MacroStrategy:
+    def execute_macros(self, macros):
+        pass
+
+
+class DefaultMacroStrategy(MacroStrategy):
+    def execute_macros(self, macros):
+        pass
+
+
+class SnippetStrategy:
+    def apply_snippets(self, snippets):
+        pass
+
+
+class DefaultSnippetStrategy(SnippetStrategy):
+    def apply_snippets(self, snippets):
+        pass
+
+
+class TextEditor:
+    def __init__(self, file_content="", encoding="utf-8"):
+        self.file_content = file_content
+        self.encoding = encoding
+        self.syntax_highlighter_strategy = None
+        self.bookmark_strategy = None
+        self.macro_strategy = None
+        self.snippet_strategy = None
+        self.hint_strategy = None
+        self.database_strategy = None
+        self.hints = []
+        self.bookmarks = []
+        self.macros = []
+        self.snippets = []
+
+    def set_syntax_highlighter_strategy(self, syntax_highlighter_strategy):
+        self.syntax_highlighter_strategy = syntax_highlighter_strategy
+
+    def set_bookmark_strategy(self, bookmark_strategy):
+        self.bookmark_strategy = bookmark_strategy
+
+    def set_macro_strategy(self, macro_strategy):
+        self.macro_strategy = macro_strategy
+
+    def set_snippet_strategy(self, snippet_strategy):
+        self.snippet_strategy = snippet_strategy
+
+    def set_hint_strategy(self, hint_strategy):
+        self.hint_strategy = hint_strategy
+
+    def set_database_strategy(self, database_strategy):
+        self.database_strategy = database_strategy
+
+    def open_file(self, file_path, encoding):
+        self.file_content = read_file(file_path, encoding)
+        self.encoding = encoding
+
+    def save_file(self, file_path):
+        save_file(file_path, self.file_content, self.encoding)
+
+    def edit_text(self, changes):
+        pass
+
+    def execute_syntax_highlighting(self):
+        if self.syntax_highlighter_strategy:
+            self.syntax_highlighter_strategy.highlight(self.file_content)
+
+    def process_bookmarks(self):
+        if self.bookmark_strategy:
+            self.bookmark_strategy.process_bookmarks(self.bookmarks)
+
+    def execute_macros(self):
+        if self.macro_strategy:
+            self.macro_strategy.execute_macros(self.macros)
+
+    def apply_snippets(self):
+        if self.snippet_strategy:
+            self.snippet_strategy.apply_snippets(self.snippets)
+
+    def get_hints(self):
+        if self.hint_strategy:
+            self.hints = self.hint_strategy.get_hints(self.hints)
+            return self.hints
+
+    def save_to_database(self):
+        if self.database_strategy:
+            self.database_strategy.connect({
+                "dbname": "trpz",
+                "user": "postgres",
+                "password": "postgres",
+                "host": "localhost",
+                "port": "5432"
+            })
+            self.database_strategy.save_to_database(self.file_content)
+
+
+def read_file(file_path, encoding):
+    with open(file_path, 'r', encoding=encoding) as file:
+        return file.read()
+
+
+def save_file(file_path, content, encoding):
+    with open(file_path, 'w', encoding=encoding) as file:
+        file.write(content)
